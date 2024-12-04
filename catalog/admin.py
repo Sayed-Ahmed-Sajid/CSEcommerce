a96f56c18f75005d9a44f8f86ae47b1917908109
sayed ahmed sajid <sayedahmedsajid2023@gmail.com>
2024-12-05 02:35:22 +0600

    first commit

---
+++ catalog/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import Item, Order, OrderItem
+
+
+
+
+
+# Register your models here.
+
+
+
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug' : ('title',)} 
+    list_display=[
+        'title',
+        'price',
+        'discount_price'
+    ]
+
+admin.site.register(Item,ItemAdmin)
+admin.site.register(Order)
+admin.site.register(OrderItem)
